90ad9f44f874432085ac8a3b7d38f10b908f68fb
marcinlucko <marcin.lucko@me.com>
2021-08-31 21:30:01 +0200

    change owner

--- frontend/utils/contracts.js
+++ frontend/utils/contracts.js
@@ -1,6 +1,6 @@
 import { loadContract } from "@utils/chain";
-import BEP20TokenAbi from "@abis/BEP20Token.json";
-import ETBStakingAbi from "@abis/ETBStaking.json";
+import BEP20TokenAbi from "@abis/BEP20Token";
+import ETBStakingAbi from "@abis/ETBStaking";
 
 export const BEP20Token = (chainId) => loadContract(BEP20TokenAbi, chainId);
 export const ETBStaking = (chainId) => loadContract(ETBStakingAbi, chainId);
